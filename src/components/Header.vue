d31cf921fc41a331a407e8fe474dfee38ddbb664
Jakob Varenbud <103386301+jakob-varenbud@users.noreply.github.com>
2024-02-08 09:49:39 +0100

    desktop header comp ready
    responisve adjustments in the next step

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,71 +1,76 @@
 <template>
-  <div class="min-h-svh w-svw flex justify-between">
-    <div class="flex flex-col justify-center w-1/2">
-      <h1>DRS Smart Repair</h1>
-      <h2 class="mb-4">Kleine Schäden schnell beheben</h2>
-      <div class="flex items-center space-x-2">
-        <img
-          class="h-6 w-6"
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
-          alt="Häkchen Icon"
-        />
-        <div>Für Parkschäden, Dellen, Kratzer, Steinschläge und Innenraum</div>
+  <div class="container size-full max-w-7xl ml-auto mr-auto">
+    <div class="height-hero flex justify-between gap-12">
+      <div class="flex flex-col justify-center w-1/2">
+        <h1>DRS Smart Repair</h1>
+        <h2 class="mb-4">Kleine Schäden schnell beheben</h2>
+        <div class="flex items-center space-x-2">
+          <img
+            class="h-6 w-6"
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
+            alt="Häkchen Icon"
+          />
+          <div>
+            Für Parkschäden, Dellen, Kratzer, Steinschläge und Innenraum
+          </div>
+        </div>
+        <div class="flex items-center space-x-2">
+          <img
+            class="h-6 w-6"
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
+            alt="Häkchen Icon"
+          />
+          <div>Kostengünstig und schnell</div>
+        </div>
+        <div class="flex items-center space-x-2 mb-4">
+          <img
+            class="h-6 w-6"
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
+            alt="Häkchen Icon"
+          />
+          <div>Einfach Foto schicken und zum Festpreis reparieren lassen</div>
+        </div>
+        <a
+          class="bg-drs-red font-color text-white p-4 rounded-3xl flex justify-center font-bold mb-4"
+          >In 3 Schritten zur Anfrage</a
+        >
+        <div class="flex">
+          <img
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50e3b8bf9fbe2a94e92a3_top%20100.webp"
+            alt="Top 100"
+            class="h-20"
+          />
+          <img
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50c6718beef84d30933cd_dekra.webp"
+            alt="Top 100"
+            class="h-20"
+          />
+          <img
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50d3bca52f1ecc2c2118e_top100.webp"
+            alt="Top 100"
+            class="h-20"
+          />
+          <img
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50738720955c1febb2e07_klimaneurtral.webp"
+            alt="Top 100"
+            class="h-20"
+          />
+          <img
+            src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/645b5a154160e449669b3d67_top_service-new.png"
+            alt="Top 100"
+            class="h-20"
+          />
+        </div>
       </div>
-      <div class="flex items-center space-x-2">
-        <img
-          class="h-6 w-6"
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
-          alt="Häkchen Icon"
-        />
-        <div>Kostengünstig und schnell</div>
+      <div class="flex flex-col justify-center w-1/2">
+        <div
+          class="bg-cover rounded-2xl h-3/4"
+          style="
+            background-image: url('https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/659e9db5fb95e3e30645b49f_drs-smartrepair.de%20Hero%20image_4%20(1).png');
+            background-position: 95% 50%;
+          "
+        ></div>
       </div>
-      <div class="flex items-center space-x-2 mb-4">
-        <img
-          class="h-6 w-6"
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b6a406763bbe2df43a5d51_tick-circle-trans-green.svg"
-          alt="Häkchen Icon"
-        />
-        <div>Einfach Foto schicken und zum Festpreis reparieren lassen</div>
-      </div>
-      <a
-        class="bg-drs-red font-color text-white p-4 rounded-3xl flex justify-center font-bold mb-4"
-        >In 3 Schritten zur Anfrage</a
-      >
-      <div class="flex">
-        <img
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50e3b8bf9fbe2a94e92a3_top%20100.webp"
-          alt="Top 100"
-          class="h-20"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50c6718beef84d30933cd_dekra.webp"
-          alt="Top 100"
-          class="h-20"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50d3bca52f1ecc2c2118e_top100.webp"
-          alt="Top 100"
-          class="h-20"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/64b50738720955c1febb2e07_klimaneurtral.webp"
-          alt="Top 100"
-          class="h-20"
-        />
-        <img
-          src="https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/645b5a154160e449669b3d67_top_service-new.png"
-          alt="Top 100"
-          class="h-20"
-        />
-      </div>
-    </div>
-    <div class="flex flex-col justify-center w-1/2">
-      <div
-        class="bg-cover bg-center rounded-2xl h-3/4"
-        style="
-          background-image: url('https://uploads-ssl.webflow.com/642d8b6e384132be9f422e44/659e9db5fb95e3e30645b49f_drs-smartrepair.de%20Hero%20image_4%20(1).png');
-        "
-      ></div>
     </div>
   </div>
 </template>
